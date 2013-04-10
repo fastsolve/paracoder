3b8ae814cb923f630d60f57c684b7457350965a5
Xiangmin Jiao <xmjiao@gmail.com>
2013-04-09 23:31:20 -0400

    Removed hasomp.m.

--- util/private/hasomp.m
+++
@@ -1,9 +0,0 @@
-function b = hasomp( file)
-
-fid = fopen(file, 'r', 'n', 'US-ASCII');
-% Read file into memory
-cfile = fread(fid, 1000, '*char')';
-fclose(fid);
-
-b = ~isempty(regexp( cfile, '\n\s*#include\s*"omp.h"', 'match'));
-end
