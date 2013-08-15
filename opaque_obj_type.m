6a841105e0f9d7927480a1487279d8177852c070
Xiangmin Jiao <xmjiao@gmail.com>
2013-08-15 19:35:57 -0400

    Minor change.

--- opaque_obj_type.m
+++ opaque_obj_type.m
@@ -1,5 +1,5 @@
 function type = opaque_obj_type %#codegen
 %OPAQUE_OBJ_TYPE Create an opaque object for pointer to a given variable.
 
-type = coder.typeof(struct('data',coder.typeof(uint8(0),[inf,inf]), ...
+type = coder.typeof(struct('data',coder.typeof(uint8(0),[inf,1]), ...
     'type',coder.typeof('0',[1,inf]), 'nitems', int32(0)));
