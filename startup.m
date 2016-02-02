fbdd597aeda77f32e94267cc0c76a92a9062434d
Xiangmin Jiao <xiangmin.jiao@stonybrook.edu>
2016-02-01 23:00:30 -0500

    Changed to disable warning message about gcc.

--- startup.m
+++ startup.m
@@ -1,6 +1,9 @@
 % Startup script for M2C.
 % The script must be run in the current directory.
 
+warning('off', 'MATLAB:pfileOlderThanMfile');
+warning('off', 'MATLAB:mex:GccVersion') 
+
 if exist('m2c', 'file')~=2
     addpath(pwd); %#ok<*MCAP>
     addpath([pwd '/util']);
