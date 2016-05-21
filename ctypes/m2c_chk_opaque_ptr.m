cf914c4cccf27deeb4ce9ef9b63b92fc406e9dfb
Xiangmin Jiao <xmjiao@gmail.com>
2016-05-21 07:40:10 -0400

    Changed for compatability with Octave.

--- ctypes/m2c_chk_opaque_ptr.m
+++ ctypes/m2c_chk_opaque_ptr.m
@@ -2,7 +2,7 @@ function m2c_chk_opaque_ptr(ptr, parent, offset) %#codegen
 %M2C_CHK_OPAQUE_PTR  Check opaque pointer
 %   m2c_chk_opaque_ptr(ptr, parent, offset)
 
-coder.cinclude('matrix.h');
+coder.cinclude('mex.h');
 
 coder.inline('always');
 coder.ceval('M2C_CHK_OPAQUE_PTR', ptr, parent, offset);
