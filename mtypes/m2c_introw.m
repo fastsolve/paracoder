ac0f7d834969abbc34a443b11319b9e203a3057b
Xiangmin Jiao <xmjiao@gmail.com>
2016-05-30 11:38:14 -0400

    Added m2c_introw for ease of declaration of dimensions.

---
+++ mtypes/m2c_introw.m
@@ -0,0 +1,19 @@
+function type = m2c_introw(maxlen, vardim) %#codegen
+% Specifies an integer (int32) row vector. Useful for size specification.
+%    m2c_introw() specifies a variable-length column vector.
+%
+%    m2c_introw(c) or m2c_introw(c, false) specifies a fixed-length 
+%        column vector with up to c numbers, where c must be a constant.
+%
+%    m2c_introw(c, true) specifies a variable-length integer column vector 
+%         with c numbers, where c must be a constant at compile time.
+%
+% See also m2c_intmat, m2c_intvec, m2c_string
+
+if nargin==0
+    type = coder.typeof(int32(0), [1, inf]);
+elseif nargin==1 || ~vardim
+    type = coder.typeof(int32(0), [1, maxlen]);
+else
+    type = coder.typeof(int32(0), [1, maxlen], [0, 1]);
+end
