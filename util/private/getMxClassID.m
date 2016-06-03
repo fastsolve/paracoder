92f46525431231f72d37092613e017de2fcab14e
Xiangmin Jiao <xmjiao@gmail.com>
2016-06-02 20:19:26 -0400

    Added base types.

--- util/private/getMxClassID.m
+++ util/private/getMxClassID.m
@@ -1,28 +1,28 @@
-function classid = getMxClassID( str)
-switch ( str)
+function classid = getMxClassID(str)
+switch (str)
     case 'boolean_T'
         classid = 'mxLOGICAL_CLASS';
-    case 'char_T'
+    case {'char_T', 'char'}
         classid = 'mxCHAR_CLASS';
-    case {'real64_T', 'real_T'}
+    case {'real64_T', 'real_T', 'double'}
         classid = 'mxDOUBLE_CLASS';
-    case 'real32_T'
+    case {'real32_T', 'float'}
         classid = 'mxSINGLE_CLASS';
-    case 'int8_T'
+    case {'int8_T', 'signed char'}
         classid = 'mxINT8_CLASS';
-    case 'uint8_T'
+    case {'uint8_T', 'unsigned char'}
         classid = 'mxUINT8_CLASS';
-    case 'int16_T'
+    case {'int16_T', 'short', 'signed short'}
         classid = 'mxINT16_CLASS';
-    case 'uint16_T'
+    case {'uint16_T', 'unsigned short'}
         classid = 'mxUINT16_CLASS';
-    case 'int32_T'
+    case {'int32_T', 'int', 'signed int'}
         classid = 'mxINT32_CLASS';
-    case 'uint32_T'
+    case {'uint32_T', 'unsigned int'}
         classid = 'mxUINT32_CLASS';
-    case 'int64_T'
+    case {'int64_T', 'long', 'signed long'}
         classid = 'mxINT64_CLASS';
-    case 'uint64_T'
+    case {'uint64_T', 'unsigned long'}
         classid = 'mxUINT64_CLASS';
     case 'struct'
         classid = 'mxSTRUCT_CLASS';
