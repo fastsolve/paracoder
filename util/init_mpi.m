c125d4e1d51192e4409922d7abe9227674064df3
Xiangmin Jiao <xmjiao@gmail.com>
2017-04-30 13:29:16 -0400

    Added checking of init

--- util/init_mpi.m
+++ util/init_mpi.m
@@ -1,12 +1,22 @@
 function init_mpi
 % Initialize MPI
 
+persistent called__;
+if ~isempty(called__)
+    return;
+end
+called__ = true;
+
 if exist(['mpi_Initialized.' mexext], 'file') && ...
         exist(['mpi_Init.' mexext], 'file') && ...
         exist(['mpi_Comm_set_errhandler.' mexext], 'file')
     try
         if ~mpi_Initialized
             mpi_Init;
+
+            if isoctave
+                atexit('uninit_mpi')
+            end
         end
     catch
         error('mpi_Init failed.');
@@ -18,10 +28,6 @@ if exist(['mpi_Initialized.' mexext], 'file') && ...
     else
         % When running in Octave without GUI, abort on error
         mpi_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_ARE_FATAL);
-    end
-    
-    if isoctave
-        atexit('uninit_mpi')
     end
 else
     warning('MPI has not been built')
