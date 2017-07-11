1c6363df6c88e1743d7f59845a8c046d031c25ca
Xiangmin Jiao <xmjiao@gmail.com>
2017-07-11 11:08:33 -0400

    Reformatted

--- api/dense/solve/qr_qtmulti.m
+++ api/dense/solve/qr_qtmulti.m
@@ -10,37 +10,47 @@ function bs = qr_qtmulti(A, bs, ncols, V)
 %#codegen -args {coder.typeof(0,[inf,inf]), coder.typeof(0,[inf,inf]),
 %#codegen int32(0), coder.typeof(0,[inf,1])}
 
-nrows = int32(size(A,1));
-if nargin<3; ncols = int32(size(A,2)); end
+nrows = int32(size(A, 1));
+if nargin < 3; ncols = int32(size(A, 2)); end
 
-if int32(size(bs,1))<nrows
-    bs = [bs; zeros(nrows-size(bs,1),size(bs,2))];
+if int32(size(bs, 1)) < nrows
+    bs = [bs; zeros(nrows-size(bs, 1), size(bs, 2))];
 end
 
-for k=1:ncols
+for k = 1:ncols
     % bs(k:nrows,:) = bs(k:nrows,:) - 2*v*(v'*bs(k:nrows,:)),
     % where v is [V(k); A(k+1:npngs,k)]
-    if nargin>3
+    if nargin > 3
         v1 = V(k);
     else
         % Compute sqrt(1-v(2:end)'*v(2:end));
         v1 = 1;
-        for ii=k+1:nrows; v1 = v1-A(ii,k)*A(ii,k); end
-        if v1<0; v1=0; else v1 = sqrt(v1); end
+        for ii = k + 1:nrows
+            v1 = v1 - A(ii, k) * A(ii, k);
+        end
+        if v1 < 0
+            v1 = 0;
+        else
+            v1 = sqrt(v1);
+        end
     end
-    
-    for jj=1:int32(size(bs,2))
-        t2 = v1*bs(k,jj);
-        for ii=k+1:nrows; t2 = t2+A(ii,k)*bs(ii,jj); end
-        
-        t2 = t2+t2;
-        bs(k,jj) = bs(k,jj) - t2 * v1;
-        for ii=k+1:nrows; bs(ii,jj) = bs(ii,jj) - t2 * A(ii,k); end
+
+    for jj = 1:int32(size(bs, 2))
+        t2 = v1 * bs(k, jj);
+        for ii = k + 1:nrows
+            t2 = t2 + A(ii, k) * bs(ii, jj);
+        end
+
+        t2 = t2 + t2;
+        bs(k, jj) = bs(k, jj) - t2 * v1;
+        for ii = k + 1:nrows
+            bs(ii, jj) = bs(ii, jj) - t2 * A(ii, k);
+        end
     end
 end
 
-if size(bs,1)>size(A,1)
-    bs = bs(1:size(A,1),:);
+if size(bs, 1) > size(A, 1)
+    bs = bs(1:size(A, 1), :);
 end
 
 function test %#ok<DEFNU>
--- api/dense/solve/rescale_matrix.m
+++ api/dense/solve/rescale_matrix.m
@@ -1,19 +1,19 @@
-function [V, ts] = rescale_matrix(V, ncols, ts)  
+function [V, ts] = rescale_matrix(V, ncols, ts)
 %% Rescale the columns of a matrix to reduce condition number
 %#codegen -args {coder.typeof(0, [inf,inf], [1,1]), int32(0), coder.typeof(0, [inf,1], [1,0])}
-if nargin<2; 
-    ncols=int32(size(V,2)); 
+if nargin < 2
+    ncols=int32(size(V,2));
 end
-if nargin<3; 
-    ts = coder.nullcopy(zeros(ncols,1)); 
+if nargin < 3
+    ts = coder.nullcopy(zeros(ncols,1));
 else
     assert(length(ts)>=ncols);
 end
 
 for ii=1:ncols
     v = V(:,ii);
-    ts(ii) = norm2_vec(v);
-    
+    ts(ii) = vec_norm2(v);
+
     if abs(ts(ii)) == 0
         ts(ii)=1;
     else
