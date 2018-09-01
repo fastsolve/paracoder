764aa7e5df1f190b99a9e5270ca545b1b06c2119
Xiangmin Jiao <xiangmin.jiao@stonybrook.edu>
2018-08-31 23:52:40 -0400

    renamed sqnorm2_vec to vec_sqnorm2

--- api/dense/tiny/qr3x3.m
+++ api/dense/tiny/qr3x3.m
@@ -7,12 +7,12 @@ function [A,R] = qr3x3(A) %#codegen
 coder.inline('always');
 
 R = coder.nullcopy(zeros(3,3));
-R(1,1) = sqrt(sqnorm2_vec(A(:,1)));
+R(1,1) = sqrt(vec_sqnorm2(A(:,1)));
 A(:,1) = A(:,1)/R(1,1);
 R(1,2) = A(:,1)'*A(:,2);
 
 A(:,2) = A(:,2) - R(1,2)*A(:,1);
-R(2,2) = sqrt(sqnorm2_vec(A(:,2)));
+R(2,2) = sqrt(vec_sqnorm2(A(:,2)));
 A(:,2) = A(:,2)/R(2,2);
 
 R(1,3) = A(:,1)'*A(:,3);
@@ -20,7 +20,7 @@ A(:,3) = A(:,3) - R(1,3)*A(:,1);
 R(2,3) = A(:,2)'*A(:,3);
 
 A(:,3) = A(:,3) - R(2,3)*A(:,2);
-R(3,3) = sqrt(sqnorm2_vec(A(:,3)));
+R(3,3) = sqrt(vec_sqnorm2(A(:,3)));
 A(:,3) = A(:,3)/R(3,3);
 
 function test %#ok<DEFNU>
