6478fcc4bca97e0358de828b9b2ca45fd46f4c49
Xiangmin Jiao <xmjiao@gmail.com>
2021-06-09 11:48:05 -0400

    suppress warning of shadowed function for Octave

--- startup.m
+++ startup.m
@@ -1,6 +1,7 @@
 % Startup script for M2C.
 if exist('OCTAVE_VERSION', 'builtin')
     more off;
+    warning('off', 'Octave:shadowed-function');
 else
     warning('off', 'MATLAB:mex:GccVersion');
     warning('off', 'MATLAB:mex:GccVersion_link');
